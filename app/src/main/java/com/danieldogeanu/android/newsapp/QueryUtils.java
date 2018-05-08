60c75c71073054ea778f21aac403fd0dd319c211
Daniel Dogeanu <contact@danieldogeanu.com>
2018-05-08 22:39:51 +0300

    Added QueryUtils Class

---
+++ app/src/main/java/com/danieldogeanu/android/newsapp/QueryUtils.java
@@ -0,0 +1,121 @@
+package com.danieldogeanu.android.newsapp;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class QueryUtils {
+
+    private static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
+    private QueryUtils() {}
+
+    public static List<Article> fetchNews(String requestUrl) {
+        URL url = createUrl(requestUrl);
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+        }
+        return extractArticleFromJson(jsonResponse);
+    }
+
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error creating URL.", e);
+        }
+        return url;
+    }
+
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+        if (url == null) return jsonResponse;
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the JSON results.", e);
+        } finally {
+            if (urlConnection != null) urlConnection.disconnect();
+            if (inputStream != null) inputStream.close();
+        }
+        return jsonResponse;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    private static List<Article> extractArticleFromJson(String stringJson) {
+        if (TextUtils.isEmpty(stringJson)) return null;
+
+        List<Article> articles = new ArrayList<>();
+        try {
+            JSONObject baseJsonResponse = new JSONObject(stringJson);
+            JSONObject jsonResponse = baseJsonResponse.getJSONObject("response");
+            String status = jsonResponse.getString("status");
+
+            if (status.equals("ok")) {
+                JSONArray articleArray = jsonResponse.getJSONArray("results");
+
+                for (int i = 0; i < articleArray.length(); i++) {
+                    JSONObject currentArticle = articleArray.getJSONObject(i);
+
+                    String published = currentArticle.getString("webPublicationDate");
+                    String title = currentArticle.getString("webTitle");
+                    String url = currentArticle.getString("webUrl");
+                    String thumbnail = currentArticle.getJSONObject("fields").getString("thumbnail");
+
+                    Article article = new Article(title, url, thumbnail, published);
+                    articles.add(article);
+                }
+            }
+
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Problem parsing JSON results.", e);
+        }
+        return articles;
+    }
+
+}
