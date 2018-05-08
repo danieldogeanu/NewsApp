a8a4589297aa43e67ea444d133a2520d7e0fef32
Daniel Dogeanu <contact@danieldogeanu.com>
2018-05-08 03:58:28 +0300

    Populated ListView with Placeholder Articles

--- app/src/main/java/com/danieldogeanu/android/newsapp/NewsActivity.java
+++ app/src/main/java/com/danieldogeanu/android/newsapp/NewsActivity.java
@@ -1,7 +1,10 @@
 package com.danieldogeanu.android.newsapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NewsActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class NewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        ArrayList<Article> articles = new ArrayList<>();
+        String placeholderHeadline = getString(R.string.placeholder_headline);
+        for (int i = 0; i < 10; i++) {
+            articles.add(new Article(placeholderHeadline, ""));
+        }
+
+        ListView listView = (ListView) findViewById(R.id.newsList);
+        ArticleAdapter adapter = new ArticleAdapter(this, articles);
+        listView.setAdapter(adapter);
     }
 }
