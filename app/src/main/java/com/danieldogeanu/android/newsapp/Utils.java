541cb23f983735c7f287383c659fb539dcbf83fe
Daniel Dogeanu <contact@danieldogeanu.com>
2018-05-15 08:15:22 +0300

    Fixed String Index Out Of Bounds Exception in Capitalize Method

--- app/src/main/java/com/danieldogeanu/android/newsapp/Utils.java
+++ app/src/main/java/com/danieldogeanu/android/newsapp/Utils.java
@@ -22,7 +22,9 @@ public final class Utils {
         String[] strArray = string.split(" ");
         StringBuilder builder = new StringBuilder();
         for (String s : strArray) {
-            String cap = s.substring(0, 1).toUpperCase() + s.substring(1);
+            String cap;
+            if (s.length() > 1) cap = s.substring(0, 1).toUpperCase() + s.substring(1);
+            else cap = s.toUpperCase();
             builder.append(cap).append(" ");
         }
         return builder.toString();
